aa07a31f86a6b5afbaa289abe933fad6a5390c40
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-21 13:07:12 +0900

    :alembic: ページを編集してみる

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let title = '楽しいSvelteKit';
+</script>
+
+<header>
+	<h1>{title}</h1>
+	<p>HTMLとCSSとJavascriptでつくるWebの世界</p>
+</header>
+
+<main>
+	<div class="hello">Hello World</div>
+</main>
+
+<footer>
+	<p>Created by fukMnk</p>
+</footer>
+
+<style>
+	h1 {
+		font-size: 2em;
+	}
+</style>
